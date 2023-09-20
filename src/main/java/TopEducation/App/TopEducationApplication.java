0acf5281034869a043e3a501e0fecc6358895378
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2023-09-20 20:10:30 -0300

    Initial Commit
    Spring application Initialized

---
+++ src/main/java/TopEducation/App/TopEducationApplication.java
@@ -0,0 +1,13 @@
+package TopEducation.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TopEducation/App/TopEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package TopEducation.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
